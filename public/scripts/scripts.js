46a183309d255f108212052a4043f54468838397
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2017-03-10 15:39:48 -0500

    moved nested ajax calls into deferred object/chained structure and created modules

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,38 +1,43 @@
 
+/*
+*
+*  Set timeout to 5 seconds if deferred object doesn't get resolved 
+*
+*/
 
-		/*
-		*
-		* Currently nesting the Darksky API call and Geocoder inside this async Navigator callback as both
-		* depend on the position data -- need to look into if chaining / using jQuery deferred maybe preferential
-		*
-		*/
+var getLocation = function(){
 
-      	if(navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
+  var deferred = $.Deferred();
+
+   if(navigator.geolocation) {
+		     navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, { timeout: 5000 });
+   		 } else {
+   		 	   deferred.reject(new Error('Your browser does not support Geo Location.'));
+   		 }
+
+   		 return deferred.promise();
+   		
+   		}
+   
+
+/* 
+*
+* Create a geocode object and translate our geolocate object into a specific location
+*
+*/ 
+
+var geoCode = function(geolocate_coords){
+
+	     var geocoder = new google.maps.Geocoder();     
 
 
-                /*
-				*	grab the lat, long from the async navigator interface position object 
-				*	and feed into google maps' geolocate object
-				*/
-                  var lat = position.coords.latitude, 
-                  	  long = position.coords.longitude,
-                  	  coords = lat + "," + long,
-                      geocoder = new google.maps.Geocoder(),      
-                  	  geolocate = new google.maps.LatLng(lat, long);
-
-
-                  /* 
-                  *
-                  * Use the geocode method to convert our Geolocate object into a specific location
-                  *
-                  */ 
-                  geocoder.geocode({'latLng': geolocate}, function(results, status) {
+             geocoder.geocode({'latLng': geolocate_coords}, function(results, status) {
                     if (status == google.maps.GeocoderStatus.OK) {
-                     
-
-                       var city = results[4].address_components[0].long_name,
+                   
+                    var city = results[4].address_components[0].long_name,
                            state = results[4].address_components[1].long_name;
+
+                           console.log("helllooo");
 
                            console.log(city);
                            console.log(state);
@@ -40,48 +45,53 @@
                      	}  
                   	});    
 
-                var api_key = 'ee2ac15f04e14b6646dc6508f06d7532';
+  
 
-				$.ajax({
+              }
+
+/* 
+*
+* Get weather data from DarkSky API based on current location from the Navigator instance
+*
+*/ 
+var getWeather = function(coords){
+
+	        var api_key = 'ee2ac15f04e14b6646dc6508f06d7532';
+
+
+			return $.ajax({
 					  url: "https://api.darksky.net/forecast/" + api_key + "/" + coords,
 					  dataType: "jsonp"
 					}).done(function(data){
 						console.log("data!");
 					  	console.log(data);
-					  	$("body").append("<div>" + data.currently.temperature + "</div>");
-					  		  	$("body").append("<div>" + data.currently.summary + "</div>");
+					  	$("body").append("<div>" + data.currently.temperature + "</div>").append("<div>" + data.currently.summary + "</div>");
+                	});
+
+		}
 
 
 
-                });
+/*
+*
+* The weather from the DarkSky API as well as the Geocoder depend on the Navigator instance's location data, 
+* so we are turning this async object into a deferred promise that will resolve before we make our two other  
+* ajax calls that depend on the location data
+*
+*/
 
+$.when(getLocation()).then(function(data){
 
+		console.log(data);
+		 var lat = data.coords.latitude, 
+                  	  long = data.coords.longitude,
+                  	  coords = lat + "," + long,
+                  	  geolocate = new google.maps.LatLng(lat, long);
 
+		getWeather(coords);
+		geoCode(geolocate);
 
-});
-
-
-
-
-
-                }
-
-
-               var word = "orange";
-
-
-        $.get(
-    'https://de.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=' + word + '&callback=?',
-    function (response) {
-        var resp = $.parseJSON(response);
-        var text = resp.parse.text['*'];
-        re = /img.*?src="(.*?)"/g
-        while( match = re.exec(text)) { 
-        	console.log("wikie");
-        	console.log(response);
-           console.log(match[1]); 
-        }
-});
+	});
 
 
 
